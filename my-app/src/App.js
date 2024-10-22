5d81d54ae883db7eacdd652029101aa27afa5444
Ethan <Emwolf16@yahoo.com>
2024-10-21 19:10:35 -0500

    changed default react page

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <button class="switch"></button>
+        <p class="timer">00:00</p>
+        <button class="btn">Start</button>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Click button to start pomodoro!
         </p>
-        <a
+       <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/EthanWolf714"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+         Link to Github
         </a>
       </header>
     </div>
+        
   );
 }
 
